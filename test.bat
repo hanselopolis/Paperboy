83f81eb95aeb30743a70b5148c3bc50dacf1df0b
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-07 22:34:53 -0500

    Update test.bat - correct bat del file name

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell.exe -Command "Start-Sleep -Seconds 3"
-powershell.exe -Command "rm paperduck_wip.ps1"
-del "test - Copy.bat"
+powershell.exe -Command "rm paperboy.exe"
+del "test.bat"
 pause
