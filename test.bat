d83325ec6d52abe91f35cd65812a72ff0a963d25
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-07 23:16:09 -0500

    change sleep time = 5sec

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell.exe -Command "Start-Sleep -Seconds 3"
+powershell.exe -Command "Start-Sleep -Seconds 5"
 powershell.exe -Command "rm paperboy.exe"
 powershell.exe -Command ".\paperduck_error.vbs"
 del "test.bat"
