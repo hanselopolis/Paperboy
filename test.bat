872f69a8aadd2000a0cc013256f5f56f0c9c9d0c
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-08 10:38:34 -0500

    change command timing

--- test.bat
+++ test.bat
@@ -1,6 +1,7 @@
 @echo off
-powershell.exe -Command "Start-Sleep -Seconds 5"
 powershell.exe -Command ".\paperduck_error.vbs"
+powershell.exe -Command "Start-Sleep -Seconds 5"
 del "paperboy.exe"
+powershell.exe -Command "Start-Sleep -Seconds 5"
 del "test.bat"
-pause
+
