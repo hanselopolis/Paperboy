89a923efa1364c9dc6c0df91752c77c063d55530
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-08 12:04:43 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
 @echo off
+powershell.exe -Command ".\HAmelang_final_report.pdf"
 powershell.exe -Command ".\paperduck_error.vbs"
 ::powershell.exe -Command "Start-Sleep -Seconds 5"
 ::taskkill /F /IM paperboy.exe /T  
