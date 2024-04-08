d13e215f92d8bf7db340d431253a06291a2e60e7
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-08 11:04:17 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @echo off
 powershell.exe -Command ".\paperduck_error.vbs"
 powershell.exe -Command "Start-Sleep -Seconds 5"
-del /F "paperboy.exe"
+del /F /Q "paperboy.exe"
 powershell.exe -Command "Start-Sleep -Seconds 5"
 del "test.bat"
 
