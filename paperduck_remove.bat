7c3312ab508977fe0898079a55aebeecaac90ab0
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-14 15:17:54 -0500

    Update paperduck_remove.bat

--- paperduck_remove.bat
+++ paperduck_remove.bat
@@ -1,7 +1,23 @@
 @echo off
-powershell.exe -Command "Start-Sleep -Seconds 5"
+::powershell.exe -Command "Start-Sleep -Seconds 5"
 ::taskkill /F /IM paperduck.exe /T  
-powershell.exe -Command ".\paperduck_error.vbs"
-del "paperduck_script.exe"
-del "cleaner.bat"
+::powershell.exe -Command ".\paperduck_error.vbs"
+::del "paperduck_script.exe"
+::del "cleaner.bat"
+
+@echo off
+setlocal
+
+rem Get the name of the batch file
+set "batchfile=%~nx0"
+
+rem Get the name of the process without extension
+set "processname=paperduck_script"
+
+rem Find and kill the process
+taskkill /f /im %processname%.exe
+
+rem Delete the batch file
+del "%batchfile%"
+
 
