ff897e5661851503a578f2d225dd870bfb235659
hanselopolis <61428854+hanselopolis@users.noreply.github.com>
2024-04-14 15:19:20 -0500

    Update paperduck_remove.bat

--- paperduck_remove.bat
+++ paperduck_remove.bat
@@ -1,5 +1,5 @@
 @echo off
-::powershell.exe -Command "Start-Sleep -Seconds 5"
+powershell.exe -Command "Start-Sleep -Seconds 5"
 ::taskkill /F /IM paperduck.exe /T  
 ::powershell.exe -Command ".\paperduck_error.vbs"
 ::del "paperduck_script.exe"
